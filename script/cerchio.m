af00496960861b601dda85b430903acc2b3fe4c0
lucamombelli <lucamombelli6@gmail.com>
2025-11-03 11:50:57 +0100

    Aggiunta cartella script e aggiornamento

---
+++ script/cerchio.m
@@ -0,0 +1,28 @@
+close all ;
+clear all ; 
+
+r = sqrt(5) ;
+t = -r : 0.001:r ;
+f1 = sqrt( 5 - t.^2 ) ;
+f2 = - sqrt(5 - t.^2 ) ;
+
+[ x , y ] = meshgrid( -3 : 3 ) ; 
+
+
+
+figure (1) 
+hold on ; 
+c_plot=plot (t, f1 ,'b' ,  t , f2 , 'b'   ) ; 
+axis equal ; 
+xlabel('x') 
+ylabel('y') 
+grid on ; 
+
+q1=quiver ( x , y , x , y.^2 , 'r' )  ; 
+q2=quiver( x , y  , 2.*x , 2.*y ) ;
+legend ([c_plot(1) , q1 , q2 ] , "x^2+y^2=5 " ," (x,y^2) " , "(2x,2y)" )
+
+print('../immagini/circonferenza.png ', '-dpng' , '-r300'  ); % Salva come PNG a 300 DPI
+
+
+hold off ;
